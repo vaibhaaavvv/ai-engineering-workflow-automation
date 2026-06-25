49d229d430e5b0cd61ac7e57c9efdd98ccbcf2da
Vaibhav <22838.vaibhav@gmail.com>
2026-06-25 13:12:44 -0400

    pingable endpoint

--- backend/src/main/java/com/knowledge/assistant/controller/AuthController.java
+++ backend/src/main/java/com/knowledge/assistant/controller/AuthController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,11 @@ public class AuthController {
     @Autowired
     public AuthController(AuthService authService) {
         this.authService = authService;
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("ok");
     }
 
     @PostMapping("/signup")
